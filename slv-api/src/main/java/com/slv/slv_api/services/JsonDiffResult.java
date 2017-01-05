56a3a7fb3184cb99e81c831f925cb9d8bd12d8ee
Alexis TRAN <atran@celad2.celad.com>
2017-01-05 17:06:30 +0100

    Refacto service de diff pour Json

---
+++ slv-api/src/main/java/com/slv/slv_api/services/JsonDiffResult.java
@@ -0,0 +1,46 @@
+package com.slv.slv_api.services;
+
+/**
+ * Result of {@link JsonDiffService}. Contains information on the comparison between two Json {@link String}.
+ * @author atran
+ *
+ */
+public class JsonDiffResult {
+
+	/**
+	 * <p>Indicate if the result of the comparison: <code>true</code> if Json attributes are equals in the two Json {@link String}, <code>false</code> otherwise</p>
+	 */
+	private boolean equals;
+	
+	/**
+	 * Error message. Blank if {@link #equals} is <code>true</code>.
+	 */
+	private String errorMessage;
+	
+	/**
+	 * Constructore of {@link JsonDiffResult}
+	 * @param equals status of the comparison : <code>true</code> if Json attributes are equals in the two Json {@link String}, <code>false</code> otherwise
+	 * @param errorMessage Blank if {@link #equals} is <code>true</code>, otherwise formatted message containing information on the differences
+	 */
+	public JsonDiffResult(boolean equals, String errorMessage) {
+		super();
+		this.equals = equals;
+		this.errorMessage = errorMessage;
+	}
+
+	public boolean isEquals() {
+		return equals;
+	}
+
+	public void setEquals(boolean equals) {
+		this.equals = equals;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+}
--- slv-api/src/main/java/com/slv/slv_api/services/JsonDiffService.java
+++ slv-api/src/main/java/com/slv/slv_api/services/JsonDiffService.java
@@ -1,15 +1,37 @@
 package com.slv.slv_api.services;
 
 /**
+ * Used to compare two JSON {@link String}.
  * Interface to make a comparison of two items according to their data format.
  * 
  * @author cmarchive
+ * 
+ * @return 
  */
-public class JsonDiffService implements DiffService {
+public class JsonDiffService  {
 
-	@Override
-	public void diff() {
-		
+	/**
+	 * Instance of {@link JsonDiffService}
+	 */
+	private JsonDiffService service = null;
+	
+	/**
+	 * @return instance of {@link JsonDiffService} as a Singleton
+	 */
+	public JsonDiffService getInstance() {
+		if(service == null) {
+			service = new JsonDiffService();
+		}
+		return service;
+	}
+	
+	/**
+	 * Method that verifies if two {@link String} representing two Json streams are equals in terms of attributes
+	 * @return {@link JsonDiffResult} an object that indicates the result of the comparison and an error message when needed
+	 */
+	public JsonDiffResult diff(String toVerify, String target) {
+		//TODO - Implement this method
+		return null;
 	}
 
 }
