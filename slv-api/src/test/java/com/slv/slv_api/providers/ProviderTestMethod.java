75a68f263976a4d2dff114e6350a292865780d3e
yromdhane <yromdhane@CELAD-F848B.celad2.celad.com>
2017-01-10 18:04:46 +0100

    Ajout de tests pour le module providers

---
+++ slv-api/src/test/java/com/slv/slv_api/providers/ProviderTestMethod.java
@@ -0,0 +1,48 @@
+package com.slv.slv_api.providers;
+
+import com.slv.slv_api.userprofile.JsonAttributes;
+
+/**
+ * Tests methods names of provider module to hold created providers 
+ * identifiers in a map in order to delete them
+ * 
+ * @author yromdhane
+ *
+ */
+public enum ProviderTestMethod {
+	
+	getAllProviders("getAllProviders"),
+	createProvider("createProvider"),
+	updateProvider("updateProvider"),
+	deleteProvider("deleteProvider"),
+	createProviderKoMissedName("createProviderKoMissedName"),
+	createProviderMissedPollutionRate("createProviderMissedPollutionRate"),
+	createProviderMissedTime("createProviderMissedTime"),
+	initProviderOfUpdateTests("initProviderOfUpdateTests"),
+	updateProviderMissedNewName("initProviderOfUpdateTests"),
+	updateProviderMissedProviderId("initProviderOfUpdateTests"),
+	clearProviderOfUpdateTests("initProviderOfUpdateTests")
+	;
+	
+	
+	/**
+	 * The name of the test method
+	 */
+	private String name;
+	
+	/**
+	 * Constructor to provide a {@link ProviderTestMethod} enumeration.
+	 * 
+	 * @param name The method's name
+	 */
+	private ProviderTestMethod(final String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return {@link JsonAttributes#name}
+	 */
+	public String getName() {
+		return this.name;
+	}
+}
