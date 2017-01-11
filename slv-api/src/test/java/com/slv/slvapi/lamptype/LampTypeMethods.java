928521a5dc3428f49e6decec50b7320d81081d7f
Cyril MARCHIVE <cmarchive@celad2.celad.com>
2017-01-11 11:42:29 +0100

    checkstyle

--- slv-api/src/test/java/com/slv/slvapi/lamptype/LampTypeMethods.java
+++ slv-api/src/test/java/com/slv/slvapi/lamptype/LampTypeMethods.java
@@ -1,23 +1,56 @@
 package com.slv.slvapi.lamptype;
 
-/*
-  * Rest api URLs for a lamp types
+/**
+ * Enumerates Rest API lamp type methods.
+ * 
+ * @author cmarchive
  */
 public enum LampTypeMethods {
 
-	CREATE_BRAND("assetmanagement/createBrand"),
-	UPDATE_BRAND("assetmanagement/updateBrand"),
-	DELETE_BRAND("assetmanagement/deleteBrand"),
-	GET_ALL_BRANDS("asset/getAllBrands"),
-	GET_BRAND_PROPERTY_DESCRIPTORS("assetmanagement/getBrandPropertyDescriptors");
-	
-	private String url;
-	
-	private LampTypeMethods(final String url) {
-		this.url = url;
-	}
-	
-	public String getUrl() {
-		return this.url;
-	}
-} 
+  /**
+   * URN for creat brand call.
+   */
+  CREATE_BRAND("assetmanagement/createBrand"),
+  
+  /**
+   * URN for update brand call.
+   */
+  UPDATE_BRAND("assetmanagement/updateBrand"),
+  
+  /**
+   * URN for delete brand call.
+   */
+  DELETE_BRAND("assetmanagement/deleteBrand"),
+  
+  /**
+   * URN for get all brands call.
+   */
+  GET_ALL_BRANDS("asset/getAllBrands"),
+  
+  /**
+   * URN for get brand property description call.
+   */
+  GET_BRAND_PROPERTY_DESCRIPTORS("assetmanagement/getBrandPropertyDescriptors");
+
+  /**
+   * The url.
+   */
+  private String url;
+
+  /**
+   * Constructor to provide a LampTypeMethods enumeration.
+   * 
+   * @param url
+   *          The url
+   */
+  private LampTypeMethods(final String url) {
+    this.url = url;
+  }
+
+  /**
+   * @return {@link LampTypeMethods#url}.
+   */
+  public String getUrl() {
+    return this.url;
+  }
+}
